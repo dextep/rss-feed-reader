6de77e4349ac819074788690199a691b1b7595bb
Grzegorz <grzegorz.popiel@yahoo.com>
2020-07-08 20:02:58 +0200

    Second commit. Added sass and some styles.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import FeedPreview from './components/FeedPreview/index'
 function App() {
   return (
     <div className="App">
-        <FeedPreview url={'https://www.gamespot.com/feeds/mashup/'}></FeedPreview>
+        <FeedPreview url="https://www.gamespot.com/feeds/mashup/"/>
+        <FeedPreview url="http://www.reddit.com/.rss"/>
+        <FeedPreview url="http://feeds.bbci.co.uk/news/rss.xml"/>
     </div>
   );
 }
--- src/components/FeedPreview/index.js
+++ src/components/FeedPreview/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import './styles.css'
+import './styles.scss'
 import axios from 'axios'
 
 export default function FeedPreview (props) {
@@ -33,23 +33,54 @@ export default function FeedPreview (props) {
 
     return (
         <div>
-            <h1>
-                <img src={data.image} /> {data.title}
-            </h1>
-            {listings.length === 0 && <p>Loading...</p>}
-            {listings.slice(0, 3).map((l, i) => {
-                return (
-                    <div key={i}>
-                        <h1>{l.title}</h1>
-                        <img src={l.enclosure.link}></img>
-                        <div>
-                            <div dangerouslySetInnerHTML={{__html: l.description}} />
-                            <div dangerouslySetInnerHTML={{__html: l.content}} />
-                            <button variant="primary" onClick={openLink.bind(this, l.link)}>Open</button>{" "}
+            <div className="feeds-box">
+                <div className="feeds-box__header">
+                    <h1 className="header-primary">
+                        <span className="header-primary--title">{data.title}</span>
+                        {/*<span className="header-primary--description">{data.description}</span>*/}
+                    </h1>
+                </div>
+                <div className="feeds">
+                    {listings.length === 0 && <p>Loading...</p>}
+                    {listings.map((item, i) => {
+                        return (
+                            <div className="item" key={i}>
+                                {
+                                    typeof item.enclosure.link !== 'undefined' &&
+                                    <div className="item__img">
+                                        <img className="img" src={item.enclosure.link}/>
+                                    </div>
+                                }
+                                <div className="item__title">
+                                    <h1 className="header-secondary">
+                                        <span className="header-secondary--title">{item.title}</span>
+                                    </h1>
+                                    <span className="header-secondary--description" dangerouslySetInnerHTML={{__html: item.content}} />
+                                    <p>{item.pubDate}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+
+                {/*<h1>
+                    <img src={data.image} /> {data.title}
+                </h1>
+                {listings.length === 0 && <p>Loading...</p>}
+                {listings.slice(0, 3).map((l, i) => {
+                    return (
+                        <div key={i}>
+                            <h1>{l.title}</h1>
+                            <img src={l.enclosure.link}></img>
+                            <div>
+                                <div dangerouslySetInnerHTML={{__html: l.description}} />
+                                <div dangerouslySetInnerHTML={{__html: l.content}} />
+                                <button variant="primary" onClick={openLink.bind(this, l.link)}>Open</button>{" "}
+                            </div>
                         </div>
-                    </div>
-                );
-            })}
+                    );
+                })}*/}
         </div>
     );
 }
