412a94ad5c70cfa067a03837e4f1fc38e2e79b0b
dextep <grzegorz.popiel@yahoo.com>
2020-07-10 18:31:15 +0200

    Added time diff.

--- src/components/FeedPreview/index.js
+++ src/components/FeedPreview/index.js
@@ -35,6 +35,30 @@ export default function FeedPreview (props) {
         }
     }, [initialized, url]);
 
+    function timeDiff(curr, prev) {
+        var ms_Min = 60 * 1000;     // milliseconds in Minute
+        var ms_Hour = ms_Min * 60;  // milliseconds in Hour
+        var ms_Day = ms_Hour * 24;  // milliseconds in day
+        var ms_Mon = ms_Day * 30;   // milliseconds in Month
+        var ms_Yr = ms_Day * 365;   // milliseconds in Year
+        var diff = curr - prev;     //difference between dates.
+
+        // If the diff is less then milliseconds in a minute
+        if (diff < ms_Min) {
+            return Math.round(diff / 1000) + ' seconds ago';
+        } else if (diff < ms_Hour) {
+            return Math.round(diff / ms_Min) + ' minutes ago';
+        } else if (diff < ms_Day) {
+            return Math.round(diff / ms_Hour) + ' hours ago';
+        } else if (diff < ms_Mon) {
+            return 'Around ' + Math.round(diff / ms_Day) + ' days ago';
+        } else if (diff < ms_Yr) {
+            return 'Around ' + Math.round(diff / ms_Mon) + ' months ago';
+        } else {
+            return 'Around ' + Math.round(diff / ms_Yr) + ' years ago';
+        }
+    }
+
     return (
         <div>
             <div className="feeds-box">
@@ -65,21 +89,21 @@ export default function FeedPreview (props) {
                             listings.filter( item => item.title.toLowerCase().includes(searching.toLowerCase())).map((item, i) => {
                                 return (
                                     <div className="item"  onClick={() => window.open(item.link)} key={i}>
-                                    {
-                                        typeof item.enclosure.link !== 'undefined' &&
-                                        <div className="item__img">
-                                            <img className="img" src={item.enclosure.link} alt={"Preview"}/>
+                                        {
+                                            typeof item.enclosure.link !== 'undefined' &&
+                                            <div className="item__img">
+                                                <img className="img" src={item.enclosure.link} alt={"Preview"}/>
+                                            </div>
+                                        }
+                                        <div className="item__title">
+                                            <h1 className="header-secondary">
+                                                <span className="header-secondary--title">{item.title}</span>
+                                            </h1>
+                                            {/*<h1 className="header-secondary">*/}
+                                            {/*    <span className="header-secondary--description" dangerouslySetInnerHTML={{__html: item.content}} />*/}
+                                            {/*</h1>*/}
+                                            <p className="header-secondary--date">{timeDiff(new Date(),new Date(item.pubDate))}</p>
                                         </div>
-                                    }
-                                    <div className="item__title">
-                                    <h1 className="header-secondary">
-                                    <span className="header-secondary--title">{item.title}</span>
-                                    </h1>
-                                    {/*<h1 className="header-secondary">*/}
-                                    {/*    <span className="header-secondary--description" dangerouslySetInnerHTML={{__html: item.content}} />*/}
-                                    {/*</h1>*/}
-                                    <p className="header-secondary--date">{item.pubDate}</p>
-                                    </div>
                                     </div>
                                 );
                             })
